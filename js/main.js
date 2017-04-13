c3d7f15673b03d953aa60409c762e8c71df4f677
Ross Correia <rmcorre@gmail.com>
2017-04-13 13:54:54 +0100

    First commit of working app

---
+++ js/main.js
@@ -0,0 +1,57 @@
+
+var channels = ["ESL_SC2", "OgamingSC2", "freecodecamp", "brunofin", "comster404"];
+var logo = "";
+
+$.each(channels, function getUsers(index, value) {
+		$.ajax({
+			type: "GET",
+			url: "https://api.twitch.tv/kraken/users/" + value,
+			data: {format: "json", client_id: "m8s9p3rmmtcy0ob9673050zcok5rf7"},
+			dataType: "json",
+			error: function(xhr) {
+    					console.log(xhr);
+							if (xhr.responseJSON.status === 422 || xhr.responseJSON.status === 404) {
+								$(".streams").append(
+	    						"<tr class='table-danger'>" +
+	    							"<td><i class='logo fa fa-question fa-2x img-thumbnail text-center bg-danger'></i></td>" +
+	    							"<td class='align-middle'><a class='text-danger' href='#'>" + value + "</a></td>" +
+	    							"<td class='align-middle'><p class='d-inline font-italic text-danger'>" + xhr.responseJSON.message + "</p></td>" +
+	    						"</tr>"
+	    					);
+							} 
+    				 },
+			success: function getStreams(userData) {
+    						console.log(userData);
+    						//logo = userData.logo;
+	    					$.ajax({
+									type: "GET",
+									url: "https://api.twitch.tv/kraken/streams/" + value,
+									data: {format: "json", client_id: "m8s9p3rmmtcy0ob9673050zcok5rf7"},
+									dataType: "json",
+									error: function(streamData) {
+													
+						    				 },
+									success: function (streamData) {
+														console.log(streamData);
+														if (streamData.stream === null) {
+							    						$(".streams").append(
+								    						"<tr class='table-warning'>" +
+								    							"<td><img class='logo img-fluid img-thumbnail' src='" + userData.logo + "'></td>" +
+								    							"<td class='align-middle'><a class='text-warning' href='" + userData.url +"'>" + userData.display_name + "</a></td>" +
+								    							"<td class='align-middle'><p class='d-inline font-italic text-warning'>Offline</p></td>" +
+								    						"</tr>"
+							    						);
+							    					}	else {
+							    						$(".streams").append(
+								    						"<tr class='table-success'>" +
+								    							"<td><img class='logo img-fluid img-thumbnail' src='" + streamData.stream.channel.logo + "'></td>" +
+								    							"<td class='align-middle'><a class='text-success' href='" + streamData.stream.channel.url +"'>" + streamData.stream.channel.display_name + "</a></td>" +
+								    							"<td class='align-middle'><p class='d-inline font-italic text-success'>" + streamData.stream.channel.status + "</p></td>" +
+								    						"</tr>"
+								    					);	
+							    					}						    												    				
+						    					 },
+								});
+    					 },
+		});
+});
